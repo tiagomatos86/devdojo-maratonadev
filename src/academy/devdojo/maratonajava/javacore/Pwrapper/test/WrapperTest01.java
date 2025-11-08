c5855033adcc48d755577e16f69196ce8f577ba3
tiagomatos86 <tiago_m_lima@hotmail.com>
2025-11-07 23:59:30 -0300

    Classes utilitárias - WraÂppers pt 01

---
+++ src/academy/devdojo/maratonajava/javacore/Pwrapper/test/WrapperTest01.java
@@ -0,0 +1,26 @@
+package academy.devdojo.maratonajava.javacore.Pwrapper.test;
+
+public class WrapperTest01 {
+    public static void main(String[] args) {
+        // os oito tipos primivitos do JAVA
+        byte byteP = 1;
+        short shortP = 1;
+        int intP = 1;
+        long longP = 10L;
+        float floatP = 10.5f;
+        double doubleP = 100.5;
+        char charP = 'W';
+        boolean booleanP = true;
+
+        // Classes Wrappers encapsulam tipos primitivos e os transformam em objetos
+        Byte byteW = 1;
+        Short shortW = 1;
+        Integer intW = 1;
+        Long longW = 10L;
+        Float floatW = 10.5f;
+        Double doubleW = 100.5;
+        Character charW = 'W';
+        Boolean booleanW = true;
+
+    }
+}
