912951d04e03a0e0caf8a99a56b0dcf0ce860eef
tiagomatos86 <tiago_m_lima@hotmail.com>
2025-11-22 22:37:39 -0300

    Classes utilitárias - internacionalização de moedas com Lolcale

--- src/academy/devdojo/maratonajava/javacore/Sformatacao/test/NumberFormatTest02.java
+++ src/academy/devdojo/maratonajava/javacore/Sformatacao/test/NumberFormatTest02.java
@@ -1,35 +1,34 @@
 package academy.devdojo.maratonajava.javacore.Sformatacao.test;
 
-import java.security.UnrecoverableKeyException;
 import java.text.NumberFormat;
 import java.text.ParseException;
 import java.util.Locale;
 
-public class NumberFormatTest01 {
-    public static void main(String[] args) {
+public class NumberFormatTest02 {
+    public static void main(String[] args){
         // NumberFormat é uma classe abastrata e não pode ser instanciada, deve ser pegue com um NumberFormat.getInstance();
         Locale localeUK = new Locale("en", "UK");
         Locale localeJP = Locale.JAPAN;
         Locale localeKO = Locale.KOREA;
         Locale localeIT = Locale.ITALY;
         NumberFormat[] nfa = new NumberFormat[5]; //array de NumberFormat e não instanciacao de objeto
-        nfa[0] = NumberFormat.getInstance(); // pega o default do sistema
-        nfa[1] = NumberFormat.getInstance(localeUK);
-        nfa[2] = NumberFormat.getInstance(localeJP);
-        nfa[3] = NumberFormat.getInstance(localeKO);
-        nfa[4] = NumberFormat.getInstance(localeIT);
+        nfa[0] = NumberFormat.getCurrencyInstance(); // pega o default do sistema
+        nfa[1] = NumberFormat.getCurrencyInstance(localeUK);
+        nfa[2] = NumberFormat.getCurrencyInstance(localeJP);
+        nfa[3] = NumberFormat.getCurrencyInstance(localeKO);
+        nfa[4] = NumberFormat.getCurrencyInstance(localeIT);
 
         //valor que será formatado
-        double valor = 10_000.2142;
+        double valor = 1_000.2142;
 
         for (int i = 0; i < nfa.length; i++) {
+            System.out.println(nfa[i].getMaximumFractionDigits()); //retorna a quantidade máxima de casas deciamis usadas no padrão, tbm pode ser usado na formatação de números não monetários
             System.out.println(nfa[i].format(valor));
         }
 
-        String valorString = "1000.2142";
-        NumberFormat nfs = NumberFormat.getInstance();
+        String valorMoeda = "¤1,000.21";
         try {
-            System.out.println(nfs.parse(valorString)); //converte em número o valor string
+            System.out.println(nfa[1].parse(valorMoeda)); //converte o valor em moeda para o valor string
         } catch (ParseException e) {
             e.printStackTrace();
         }
