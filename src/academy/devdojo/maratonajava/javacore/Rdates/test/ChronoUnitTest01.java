b4afabfe0e77063ee3299842f92d0109597588b8
tiagomatos86 <tiago_m_lima@hotmail.com>
2025-12-04 23:59:37 -0300

    aulas sobre exceções - chrono unit

---
+++ src/academy/devdojo/maratonajava/javacore/Rdates/test/ChronoUnitTest01.java
@@ -0,0 +1,13 @@
+package academy.devdojo.maratonajava.javacore.Rdates.test;
+
+import java.time.LocalDateTime;
+import java.time.Month;
+import java.time.Period;
+import java.time.temporal.ChronoUnit;
+
+public class ChronoUnitTest01 {
+    public static void main(String[] args) {
+        LocalDateTime aniversarioErik = LocalDateTime.of(2015, Month.JUNE, 15, 12, 0, 0);
+        so
+    }
+}
