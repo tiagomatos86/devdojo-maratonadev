39378dfb0a77739346fd4b400d11170c87c10612
tiagomatos86 <tiago_m_lima@hotmail.com>
2025-12-16 01:35:50 -0300

    Classes Utilitárias - DateTimeFormatter pt02

--- src/academy/devdojo/maratonajava/javacore/Sformatacao/test/DateTimeFormatterTest01.java
+++ src/academy/devdojo/maratonajava/javacore/Sformatacao/test/DateTimeFormatterTest01.java
@@ -3,7 +3,9 @@ package academy.devdojo.maratonajava.javacore.Sformatacao.test;
 import academy.devdojo.maratonajava.javacore.Gassociacao.exercicio.domain.Local;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Locale;
 
 public class DateTimeFormatterTest01 {
     public static void main(String[] args) {
@@ -24,5 +26,26 @@ public class DateTimeFormatterTest01 {
         System.out.println(parse1);
         System.out.println(parse2);
         System.out.println(parse3);
+
+        //Formatando datas do tipo LocalDateTime
+        LocalDateTime now = LocalDateTime.now();
+        String s4 = now.format(DateTimeFormatter.ISO_DATE_TIME);
+        System.out.println(s4);
+
+        LocalDateTime parse4 = LocalDateTime.parse("2025-12-16T00:02:07.1893269");
+        System.out.println(parse4);
+
+        //formatando em um formato específico
+        DateTimeFormatter formatterBR = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        String dateBR = LocalDate.now().format(formatterBR);
+        System.out.println(dateBR);
+        LocalDate parseBR = LocalDate.parse("16/12/2025", formatterBR);
+        System.out.println(parseBR);
+
+        DateTimeFormatter patternGR = DateTimeFormatter.ofPattern("dd.MMMM.yyyy", Locale.GERMAN);
+        String formatGR = LocalDate.now().format(patternGR);
+        System.out.println(formatGR);
+        LocalDate parseGR = LocalDate.parse("16.Dezember.2025", patternGR);
+        System.out.println(parseGR);
     }
 }
