0c9d2a248246dacde2f174c8bc3ec7649df943ed
tiagomatos86 <tiago_m_lima@hotmail.com>
2026-01-02 01:57:39 -0300

    Configurando a view para carregar os eventos cadastrados no BD

---
+++ src/academy/devdojo/maratonajava/javacore/Uregex/test/PatternMatcherTest03Exercise.java
@@ -0,0 +1,20 @@
+package academy.devdojo.maratonajava.javacore.Uregex.test;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class PatternMatcherTest03Exercise {
+    public static void main(String[] args) {
+        //Econtrar valores hexadeciamis em um texto
+        String regex = "0[xX][\\da-fA-F]"; // procura os caracteres abc idependnete de como aparecem na string (case sensitive)
+        String text = "O Usuario admin tem id 0x1A3F, token 0xFF09 e acesso nivel B7 no sistema 0x2C";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(text);
+
+        System.out.println("Posicoes encontradas");
+        while (matcher.find()) {
+            System.out.print(matcher.start()+" "+matcher.group()+"\n");
+        }
+
+    }
+}
