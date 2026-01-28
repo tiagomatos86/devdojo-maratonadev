4f99497abb0ba9c04b3040c022e849f9dd19860f
tiagomatos86 <tiago_m_lima@hotmail.com>
2026-01-28 02:19:38 -0300

    Classes Utilitárias - NIO pt 01 - Path, Paths, Files pt 01

---
+++ src/academy/devdojo/maratonajava/javacore/Xnio/test/PathTest01.java
@@ -0,0 +1,19 @@
+package academy.devdojo.maratonajava.javacore.Xnio.test;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+// o Pacote NIO (New I/O) é uma forma mais moderna de trabalhar com arquivos e Inputs e Outputs que substitui o pacote IO
+public class PathTest01 {
+    public static void main(String[] args) {
+        Path p1 = Paths.get("D:\\Cursos\\DevDojo-MaratonaJava-ViradoNoJiraya\\maratona-java\\file.txt");
+        Path p2 = Paths.get("D:\\Cursos\\DevDojo-MaratonaJava-ViradoNoJiraya\\maratona-java","file.txt");
+        Path p3 = Paths.get("D:", "Cursos\\DevDojo-MaratonaJava-ViradoNoJiraya\\maratona-java", "file.txt");
+        Path p4 = Paths.get("D:", "Cursos", "DevDojo-MaratonaJava-ViradoNoJiraya", "maratona-java", "file.txt");
+
+        System.out.println(p1.getFileName());
+        System.out.println(p2.getFileName());
+        System.out.println(p3.getFileName());
+        System.out.println(p4.getFileName());
+    }
+}
