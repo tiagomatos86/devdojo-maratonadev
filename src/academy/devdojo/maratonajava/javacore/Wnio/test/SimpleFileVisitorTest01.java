9f8f7489cf49de504ae64e18303bf6fa1778456f
tiagomatos86 <tiago_m_lima@hotmail.com>
2026-02-08 22:59:38 -0300

    Classes Utilitárias - NIO pt 11 - SimpleFileVisitor pt 01

---
+++ src/academy/devdojo/maratonajava/javacore/Wnio/test/SimpleFileVisitorTest01.java
@@ -0,0 +1,30 @@
+package academy.devdojo.maratonajava.javacore.Wnio.test;
+
+import java.io.IOException;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
+
+class ListAllFiles extends SimpleFileVisitor<Path> {
+    @Override
+    public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) {
+        if(file.getFileName().toString().endsWith(".java")) { //vai listar só os arquivos .java
+            System.out.println(file.getFileName());
+        }
+
+        return FileVisitResult.CONTINUE;
+    }
+}
+
+public class SimpleFileVisitorTest01 {
+    public static void main(String[] args) {
+        Path root = Path.of(".");
+        try {
+            Files.walkFileTree(root, new ListAllFiles());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
