ac87cc1ac7cb1e19764e38308ba1f626fd69e98b
tiagomatos86 <tiago_m_lima@hotmail.com>
2026-06-03 05:12:12 -0300

    179 - Coleções pt20 - Map, HashMap, LinkedHashMap pt03

---
+++ src/academy/devdojo/maratonajava/javacore/Ycolecoes/test/MapTest02.java
@@ -0,0 +1,29 @@
+package academy.devdojo.maratonajava.javacore.Ycolecoes.test;
+
+import academy.devdojo.maratonajava.javacore.Ycolecoes.domain.Consumidor;
+import academy.devdojo.maratonajava.javacore.Ycolecoes.domain.Manga;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MapTest02 {
+    public static void main(String[] args) {
+        Consumidor consumidor1 = new Consumidor("Tiago Matos");
+        Consumidor consumidor2 = new Consumidor("DevDojo Academy");
+
+        Manga manga1 = new Manga(5L, "Hellsing Ultimate", 19.9);
+        Manga manga2 = new Manga(1L, "Berserk", 9.5);
+        Manga manga3 = new Manga(4L, "Pokemon", 3.2);
+        Manga manga4 = new Manga(3L, "Attack on Titan", 11.20);
+        Manga manga5 = new Manga(2L, "Dragon Ball Z", 2.99);
+
+        Map<Consumidor, Manga> consumidorManga = new HashMap<>();
+        consumidorManga.put(consumidor1, manga1);
+        consumidorManga.put(consumidor2, manga4);
+
+        for(Map.Entry<Consumidor, Manga> entry : consumidorManga.entrySet()) {
+            System.out.println(entry.getKey().getNome() + " - " + entry.getValue().getNome());
+        }
+
+    }
+}
